c64f83db249fd882fda247dfcb8bff0a01762770
Jairo Ushiña <jago86@gmail.com>
2022-07-09 00:07:22 -0500

    First commit.

---
+++ readme.md
@@ -0,0 +1,4 @@
+# Jairo Ushiña
+## Desarrollador web
+
+Sitio web de la hoja de vida de Jairo Ushiña. [Ir al sitio web](https://jairo.backfron.com).
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '10rem',
+      },
+    },
+    extend: {
+      fontFamily: {
+        'sans': ['Barlow', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+}
